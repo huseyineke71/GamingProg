f33013bb7ebeab12029672fe055590396711c294
huseyineke71 <33815162+huseyineke71@users.noreply.github.com>
2021-01-28 01:27:13 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using GamingProgram.Entities;
+using GamingProgram.Abstract;
+using GamingProgram.Concrete;
+
+
+namespace GamingProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            BasePersonManager personManager = new PersonManager();
+            personManager.Save(new Person {DateOfBirth = new DateTime(1980,25,5),FirstName ="Hüseyin",LastName ="Eker",NationalityId = "47895665699" });
+            
+
+            
+
+        }
+    }
+    
+    
+    
+}
+
+
